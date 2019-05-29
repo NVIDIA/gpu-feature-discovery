13f3f858a276782039320dc0f3e897c879badfc0
Jordan Jacobelli <jjacobelli@nvidia.com>
2019-05-29 09:18:14 -0700

    Use NFD release in tests
    Signed-off-by: Jordan Jacobelli <jjacobelli@nvidia.com>

--- tests/ci-run-e2e.sh
+++ tests/ci-run-e2e.sh
@@ -9,18 +9,14 @@ fi
 IMAGE_NAME=$1
 VERSION=$2
 CI_COMMIT_TAG=$3
-GFD_YAML_FILE=../gpu-feature-discovery-daemonset.yaml
-NFD_YAML_FILE=./nfd.yaml
+GFD_YAML_FILE="../gpu-feature-discovery-daemonset.yaml"
+NFD_YAML_FILE_URL="https://raw.githubusercontent.com/kubernetes-sigs/node-feature-discovery/v0.4.0/nfd-daemonset-combined.yaml.template"
+NFD_YAML_FILE="nfd.yaml"
+
+wget -O "${NFD_YAML_FILE}" "${NFD_YAML_FILE_URL}"
 
 sudo apt install -y python3-pip
 sudo pip3 install -r e2e-requirements.txt
-
-# Should be remove once:
-# https://github.com/kubernetes-sigs/node-feature-discovery/pull/236 is merged
-# and a new version of NFD is released
-git clone https://github.com/kubernetes-sigs/node-feature-discovery.git
-docker build --build-arg NFD_VERSION=ci -t 127.0.0.1:5000/nfd node-feature-discovery
-docker push 127.0.0.1:5000/nfd
 
 # If it's a tag
 if [ -n "$CI_COMMIT_TAG" ]; then
