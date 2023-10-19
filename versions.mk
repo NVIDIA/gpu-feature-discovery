29fc564cf5522de7925bf8a17681cd37fa3080ea
Evan Lezar <elezar@nvidia.com>
2023-10-19 10:52:54 +0200

    Bump CUDA base image version to 12.2.2
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -18,7 +18,7 @@ VERSION ?= v0.8.2
 
 vVERSION := v$(VERSION:v%=%)
 
-CUDA_VERSION := 12.2.0
+CUDA_VERSION := 12.2.2
 GOLANG_VERSION ?= 1.20.5
 
 GIT_COMMIT ?= $(shell git describe --match="" --dirty --long --always --abbrev=40 2> /dev/null || echo "")
